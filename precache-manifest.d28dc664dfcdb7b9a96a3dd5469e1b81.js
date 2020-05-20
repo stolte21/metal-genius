97ff44b3a1121a3c67ffb60ee0e6643c4e813ff2
Ross Stolte <RossStolte@gmail.com>
2020-05-20 18:28:30 -0400

    Updates

---
+++ precache-manifest.d28dc664dfcdb7b9a96a3dd5469e1b81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51c315b4bb27aee47cd21c764e84ffcd",
+    "url": "/metal-genius/index.html"
+  },
+  {
+    "revision": "3761667dd48661b6ac7f",
+    "url": "/metal-genius/static/css/2.61a570a4.chunk.css"
+  },
+  {
+    "revision": "3761667dd48661b6ac7f",
+    "url": "/metal-genius/static/js/2.8bdced05.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/metal-genius/static/js/2.8bdced05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62838f56c77b52fcbeea",
+    "url": "/metal-genius/static/js/main.98d24c98.chunk.js"
+  },
+  {
+    "revision": "a2d42a10322b9aea9d85",
+    "url": "/metal-genius/static/js/runtime-main.76f35add.js"
+  }
+]);
