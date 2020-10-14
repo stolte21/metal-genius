cc4b0d70a5bacadcb2f1642b9ffbfc1471cbbbae
Ross Stolte <RossStolte@gmail.com>
2020-10-14 10:30:01 -0400

    update readme

--- src/components/LongestWordCard.js
+++ src/components/LongestWordCard.js
@@ -14,6 +14,7 @@ const useStyles = makeStyles(theme => ({
     },
     typography: {
         display: 'flex',
+        textAlign: 'center',
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
